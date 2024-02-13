f45bba4f11c1a68f45a0e9b93da3ba94e4750291
SeabassTheFish03 <sneumannthesecond@gmail.com>
2024-02-12 22:25:47 -0500

    Beginnings of self-loops

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from manim import *
 from automata.fa.dfa import DFA
 import sys
 import json
+import numpy as np
+
+# Unit Vector and Angle Between from:
+# https://stackoverflow.com/questions/2827393/angles-between-two-n-dimensional-vectors-in-python
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+	v1_u = unit_vector(v1)
+	v2_u = unit_vector(v2)
+	return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 class DFAScene(Scene):
 	def __init__(self, rawJson):
@@ -9,11 +20,23 @@ class DFAScene(Scene):
 		self.dfa = JSONToDFA(rawJson)
 		self.rawJson = rawJson
 
+	def sift_self_transitions(self):
+		self_transitions = []
+		not_self_transitions = []
+
+		for transition in self.rawJson["transitions"]:
+			for destination in self.rawJson["transitions"][transition]:
+				if self.rawJson["transitions"][transition][destination] == transition:
+					self_transitions.append(tuple((transition, transition)))
+				else:
+					not_self_transitions.append(tuple((transition, self.rawJson["transitions"][transition][destination])))
+
+		return self_transitions, not_self_transitions
+
 	def construct(self):
 		edge_config = {
             "stroke_width": 2,
             "tip_config": {
-                "tip_shape": ArrowSquareTip,
                 "tip_length": 0.15,
             },
             (3, 4): {
@@ -23,7 +46,8 @@ class DFAScene(Scene):
         }
 
 		vertices = self.rawJson["states"]
-		edges = JSONtoManimEdges(self.rawJson)
+		loops, edges = self.sift_self_transitions()
+
 		vertex_config = {
 			"radius": 0.3,
 			"stroke_color": "green",
@@ -34,14 +58,46 @@ class DFAScene(Scene):
 		g = DiGraph(
 			vertices,
 			edges,
-			labels=False,
+			#labels=True,
+			label_fill_color=BLACK,
 			layout="kamada_kawai",
-			edge_config=edge_config,
 			vertex_type=Circle,
 			vertex_config=vertex_config
-		).scale(1.4)
+		)
+
+		label = MathTex("A DFA")
+		label.next_to(g, 2*DOWN)
+
+		loop_arcs = VGroup()
+		for loop in loops:
+			node_pos = g[loop[0]].get_center()
+			relative_pos = g.get_center() - node_pos
+
+			between = angle_between(RIGHT, relative_pos)
+
+			if between >= 0 and between < PI/2:
+				offset = UP
+			elif between >= PI/2 and between < PI:
+				offset = LEFT
+			elif between >= PI and between < 3*PI/2:
+				offset = DOWN
+			else:
+				offset = RIGHT
+
+			self_loop = CurvedArrow(
+				start_point=relative_pos,
+				end_point=relative_pos+offset,
+				fill_color=WHITE,
+				angle=4*PI/3,
+				stroke_width=5,
+			).scale(0.5)
+
+			#self_loop.next_to(g[loop[0]], unit_vector(relative_pos)*1.2)
+			loop_arcs.add(self_loop)
 
 		self.play(Create(g))
+		self.play(Create(label))
+		self.play(Create(loop_arcs))
 		self.wait()
 
 # This function just makes the edge list for displaying the dfa. It loses the input character
